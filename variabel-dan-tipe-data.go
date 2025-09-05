59aa9869593b19b23d3b55a4d156d6c2acc8a541
fasih-kode <fasih@tutanota.com>
2025-09-05 15:05:49 +0700

    First commit: hello world in Golang

--- variabel-dan-tipe-data.go
+++ variabel-dan-tipe-data.go
@@ -2,31 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	// cara 1: pakai var
-	var nama string = "Fasih"
+// cara 1: pakai var
+var nama string = "Fasih"
 
-	// cara 2: tanpa tipe (Go otomatis deteksi)
-	var umur = 41
-	var kata = "durian"
-	var des = 2.7
-
-	//cara 3: singkat (hanya di dalam funciton)
-	kota := "Indramayu"
-	jumlah := 24
-	persen := 2.5
-
-	fmt.Println(nama, umur, kota, jumlah, persen, kata, des, lokasi, namadepan, namabelakang)
-	fmt.Println("Benar = ", true)
-	fmt.Println("Salah = ", false)
-
-	// fungsi string, menghitung jumlah karakter
-	fmt.Println(len("Fasikhuddin"))
-
-	// mengambil karakter pada posisi yang ditentukan
-	fmt.Println("Fasikhuddin"[0])
-	fmt.Println("Fasikhuddin"[3])
-}
+// cara 2: tanpa tipe (Go otomatis deteksi)
+var umur = 41
+var kata = "durian"
+var des = 2.7
 
 // Gunakan := → kalau lagi di dalam function, ini yang paling umum.
 //
@@ -42,3 +24,20 @@ var (
 	namadepan    = "Fasih"
 	namabelakang = "Khuddin"
 )
+
+func main() {
+	kota := "Indramayu"
+	//cara 3: singkat (hanya di dalam funciton dan var bentuk ini harus dalam main)
+	jumlah := 24
+	persen := 2.5
+	fmt.Println(nama, umur, kota, jumlah, persen, kata, des, lokasi, namadepan, namabelakang)
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+
+	// fungsi string, menghitung jumlah karakter
+	fmt.Println(len("Fasikhuddin"))
+
+	// mengambil karakter pada posisi yang ditentukan
+	fmt.Println("Fasikhuddin"[0])
+	fmt.Println("Fasikhuddin"[3])
+}
