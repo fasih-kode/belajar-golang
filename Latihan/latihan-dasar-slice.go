6bfb9cad23813557e9943dcdaee7e3f1da157cfa
fasih-kode <fasih@tutanota.com>
2025-09-18 22:07:03 +0700

    tambah latihan

--- Latihan/latihan-dasar-slice.go
+++ Latihan/latihan-dasar-slice.go
@@ -2,17 +2,33 @@ package main
 
 import "fmt"
 
-var angka = []int{}
-
-func jumlah(angka []int) int {
-	total := 0
-	for _, v := range angka {
-		total += v
-	}
-	return total
-}
+//var angka = []int{9, 9, 12, 7}
+//
+//func jumlah(angka []int) int {
+//	total := 0
+//	for _, v := range angka {
+//		total += v
+//	}
+//	return total
+//}
+//
+//func main() {
+//	totalHasil := jumlah(angka)
+//	fmt.Println(totalHasil)
+//}
 
 func main() {
-	totalHasil := jumlah(angka)
-	fmt.Println(totalHasil)
+	var a int
+	fmt.Println("Jumlah siswa: ")
+	fmt.Scanln(&a)
+
+	// buat slice kosong
+	var nama []string
+	for i := 0; i < a; i++ {
+		var b string
+		fmt.Printf("Siswa ke%d: ", i+1)
+		fmt.Scanln(&b)
+		nama = append(nama, b)
+	}
+	fmt.Println("Daftar siswa:", nama)
 }
