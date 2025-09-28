11a8d58534bc6fb0cc9f037e67f94058594af050
fasih-kode <fasih@tutanota.com>
2025-09-28 10:46:27 +0700

    tanbah

--- Latihan/latihan-level-2.go
+++ Latihan/latihan-level-2.go
@@ -3,60 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	var nama, harga, stok string
 
-	// Jumlah data siswa
-	var jumlah int
-	fmt.Println("Masukkan jumlah data siswa: ")
-	fmt.Scanln(&jumlah)
+	var daftarBarang []map[string]string
 
-	// Map untuk menyimpan nama dan nilai
-	siswa := make(map[string]int)
+	fmt.Print("Masukkan nama barang: ")
+	fmt.Scan(&nama)
+	fmt.Print("Masukkan harga barang: ")
+	fmt.Scan(&harga)
+	fmt.Print("Masukkan stok: ")
+	fmt.Scan(&stok)
 
-	// input data siswa
-	for i := 0; i < jumlah; i++ {
-		var nama string
-		var nilai int
-		fmt.Println("Nama Siswa: ")
-		fmt.Scanln(&nama)
-		fmt.Println("Nilai Siswa: ")
-		fmt.Scanln(&nilai)
-
-		siswa[nama] = nilai
+	barangBaru := make(map[string]string{
+		"nama" : nama,
+		"harga" : harga,
+		"stok" : stok,
 	}
 
-	// Jumlahkan semua data siswa
-	fmt.Println("\n=== Data Nilai Siswa === ")
-	total := 0
-	for nama, nilai := range siswa {
-		fmt.Printf("%s: %d\n", nama, nilai)
-		total += nilai
+	daftarBarang = append(daftarBarang, barangBaru)
 
-		// Menentuka grade pakai if
-		var grade string
-		if nilai >= 90 {
-			grade = "A"
-		} else if nilai >= 75 {
-			grade = "B"
-		} else if nilai >= 60 {
-			grade = "C"
-		} else {
-			grade = "D"
-		}
-
-		// komentar pakai switc
-		switch grade {
-		case "A":
-			fmt.Println("Sangat Baik")
-		case "B":
-			fmt.Println("Bagus, Pertahankan")
-		case "C":
-			fmt.Println("Cukup, Perlu belajar lagi")
-		case "D":
-			fmt.Println("Kurang, harus lebih giat")
-		}
-	}
-
-	// hitung rata-rata
-	rata := float64(total) / float64(jumlah)
-	fmt.Printf("\nRata-rata nilai: %.2f\n", rata)
+	fmt.Print("Daftar Barang ", daftarBarang)
 }
